c41c163c91f668ffcba261d51e604fc613a42853
Kuravi Sree Vidya Lakshmi <vidyakuravi@gmail.com>
2025-12-13 17:18:51 +0530

    Add Celsius to Fahrenheit conversion function

---
+++ Build a Celsius to Fahrenheit Converter.js
@@ -0,0 +1,9 @@
+Build a Celsius to Fahrenheit Converter
+------------------------------------------
+You should create a function named convertCtoF.
+convertCtoF should take a single numeric argument, which is the temperature in Celsius.
+convertCtoF should return a number.
+
+function convertCtoF(celsius) {
+  return (celsius * 9/5) + 32;
+}
