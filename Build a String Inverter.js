9629fa7b784e2f5efd37a6cbc4de9d06980f4dd7
Kuravi Sree Vidya Lakshmi <vidyakuravi@gmail.com>
2025-12-20 15:23:34 +0530

    Create Build a String Inverter.js

---
+++ Build a String Inverter.js
@@ -0,0 +1,8 @@
+Build a String Inverter
+----------------------------
+You should create a function named reverseString that takes a string as an argument.
+The function should return the reversed string.
+
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
