fe229c6720c6e2c2d87ff10e75ece25a8fe5d483
Kuravi Sree Vidya Lakshmi <vidyakuravi@gmail.com>
2025-12-28 20:33:16 +0530

    Add Odd Fibonacci Sum Calculator
    Implement sumFibs function to calculate the sum of odd Fibonacci numbers up to a given number.

---
+++ Build an Odd Fibonacci Sum Calculator.js
@@ -0,0 +1,24 @@
+Build an Odd Fibonacci Sum Calculator
+--------------------------------------
+You should have a sumFibs function that accepts a number as an argument.
+The sumFibs function should return the sum of all odd Fibonacci numbers that are less than or equal to the given number.
+The Fibonacci sequence starts with 0 and 1, and each subsequent number is the sum of the two previous ones.
+Only the odd Fibonacci numbers should be added to the sum.
+
+function sumFibs(num) {
+  let prev = 0;
+  let curr = 1;
+  let sum = 0;
+
+  while (curr <= num) {
+    if (curr % 2 !== 0) {
+      sum += curr;
+    }
+
+    let next = prev + curr;
+    prev = curr;
+    curr = next;
+  }
+
+  return sum;
+}
